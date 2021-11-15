ad840fc97d49d270c76d92b963d29556d9568269
AspirantPy <93662553+AspirantPy@users.noreply.github.com>
2021-11-15 23:48:27 +0300

    Add files via upload

---
+++ db.py
@@ -0,0 +1,125 @@
+import sqlite3
+
+# Потокобезопасное подключение. Не открываем соединение в каждой функции, только в декораторе
+
+def ensure_connection(funct):
+
+    def inlay(*args, **kwargs):
+        with sqlite3.connect('vault.db') as conn:
+            res = funct(*args, conn=conn, **kwargs)
+        return res
+    return inlay
+
+
+@ensure_connection
+def init_db(conn, force: bool = False):
+    ''':param force: явно пересоздать все таблицы'''
+
+    c = conn.cursor()
+
+    if force:
+        c.execute('DROP TABLE IF EXISTS user_data') # Если стоит флаг force, удалим таблицу, если она уже существует. Если сразу запустить код с этим флагом, то он не упадет.
+    
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS user_data (
+            id          INTEGER PRIMARY KEY,
+            actual_name TEXT NOT NULL,
+            user_id     INTEGER NOT NULL,
+            chat_id     INTEGER NOT NULL,
+            nickname    TEXT NOT NULL,
+            answer      INTEGER,
+            rating      INTEGER,
+            score       INTEGER
+        )
+    ''')
+
+    conn.commit()
+
+
+@ensure_connection
+def signup(conn, actual_name: str, user_id: int, chat_id: int, nickname: str, answer: int, rating: int, score: int):
+
+    c = conn.cursor()
+    c.execute('SELECT actual_name, user_id, nickname FROM user_data WHERE actual_name=? OR user_id=? OR nickname=?', (actual_name, user_id, nickname))
+    result = c.fetchone()
+
+    if result: pass
+
+    else:
+        c.execute('INSERT INTO user_data (actual_name, user_id, chat_id, nickname, answer, rating, score) VALUES (?, ?, ?, ?, ?, ?, ?)', (actual_name, user_id, chat_id, nickname, answer, rating, score))
+        conn.commit()
+
+        return 'OK'
+
+
+@ensure_connection
+def get_chat_ids(conn):
+    '''produces only unique chat_ids'''
+
+    c = conn.cursor()
+    result = [chat_id[0] for chat_id in c.execute('SELECT chat_id FROM user_data')]
+
+    return set(result)
+
+
+@ensure_connection
+def write_answers(conn, user_id: int):
+
+    c = conn.cursor()
+    c.execute('UPDATE user_data SET answer=1 WHERE user_id=?', (user_id, ))
+    c.execute('SELECT nickname FROM user_data WHERE user_id=?', (user_id, ))
+
+    result = c.fetchone()
+    conn.commit()
+
+    return result[0]
+
+
+@ensure_connection
+def write_score(conn, rating: int, nickname: str):
+
+    c = conn.cursor()
+    c.execute('UPDATE user_data SET rating=? WHERE nickname=?', (rating, nickname))
+    c.execute('UPDATE user_data SET score=score+? WHERE nickname=?', (rating, nickname))
+    conn.commit()
+
+
+@ensure_connection
+def did_they_answer(conn, user_id: int):
+
+    c = conn.cursor()
+    c.execute('SELECT answer FROM user_data WHERE user_id = ?', (user_id, ))
+    result = c.fetchone()
+    print(result)
+
+    return result[0]
+
+
+@ensure_connection
+def round_rating(conn):
+
+    c = conn.cursor()
+    result1 = [nickname[0] for nickname in c.execute('SELECT nickname FROM user_data WHERE rating=?', (2, ))]
+    result2 = [nickname[0] for nickname in c.execute('SELECT nickname FROM user_data WHERE rating=?', (1, ))]
+
+    c.execute('UPDATE user_data SET answer=0, rating=0')
+    conn.commit()
+
+    return (result1, result2)
+
+
+@ensure_connection
+def total_score(conn):
+    
+    c = conn.cursor()
+    c.execute('SELECT GROUP_CONCAT(nickname), score FROM (SELECT score, nickname FROM user_data ORDER BY score, nickname) GROUP BY score;')
+    result = c.fetchall()
+
+    return result
+
+
+@ensure_connection
+def delete_score(conn):
+
+    c = conn.cursor()
+    c.execute('DROP TABLE user_data')
---
+++ main.py
@@ -0,0 +1,280 @@
+# A quiz-bot
+# Works with groups of up to 200 people, > than that and group can be converted to supergroup and additional code is needed (chat_ID changes).
+
+import constants as keys
+
+import re
+import logging
+
+from telegram import (
+    Bot,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup
+)
+
+from telegram.ext import (
+    Updater, 
+    CommandHandler, 
+    MessageHandler,
+    CallbackQueryHandler, 
+    Filters,
+    ConversationHandler,
+)
+
+# Чтобы точно знать, подключился ли бот к Телеграму или нет
+from telegram.utils.request import Request
+
+from db import (
+    init_db,
+    signup,
+    get_chat_ids,
+    did_they_answer,
+    write_answers,
+    write_score,
+    round_rating,
+    total_score,
+    delete_score
+)
+
+
+bot_token = keys.API_TOKEN
+admin_id = keys.ADMIN_ID
+set_nickname = range(3)
+current_phase = 0
+question_state = range(2)
+
+# Идентификаторы кнопок
+CALLBACK_BUTTON1_LEFT = 'callback_button1_left'
+CALLBACK_BUTTON2_CENTRE = 'callback_button2_centre'
+CALLBACK_BUTTON3_RIGHT = 'callback_button3_right'
+
+TITLES = {
+    CALLBACK_BUTTON1_LEFT: "0",
+    CALLBACK_BUTTON2_CENTRE: "1",
+    CALLBACK_BUTTON3_RIGHT: "2"
+}
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+# Каждый список внутри "keyboard" - это один горизонтальный ряд кнопок
+# Каждый элемент внутри списка - это один вертикальный столбец, сколько кнопок - столько столбцов
+def get_base_inline_keyboard():
+    keyboard = [        
+        [
+            InlineKeyboardButton(TITLES[CALLBACK_BUTTON1_LEFT], callback_data=CALLBACK_BUTTON1_LEFT),
+            InlineKeyboardButton(TITLES[CALLBACK_BUTTON2_CENTRE], callback_data=CALLBACK_BUTTON2_CENTRE),
+            InlineKeyboardButton(TITLES[CALLBACK_BUTTON3_RIGHT], callback_data=CALLBACK_BUTTON3_RIGHT)
+        ]
+    ]
+    return InlineKeyboardMarkup(keyboard)
+
+def keyboard_callback_handler(update, context, chat_data=None, **kwargs):
+    # Обработчик ВСЕХ кнопок со ВСЕХ клавиатур
+    query = update.callback_query
+    data = query.data
+
+    chat_id = update.effective_message.chat_id
+    current_text = update.effective_message.text
+    nickname = re.findall("(^.+)\s#", current_text, re.DOTALL)[0]
+
+    if data == CALLBACK_BUTTON1_LEFT:
+        query.edit_message_text(text=(current_text + "\t#оценка: 0 баллов"))
+        rating = 0
+        
+    elif data == CALLBACK_BUTTON2_CENTRE:
+        query.edit_message_text(text=(current_text + "\t#оценка: 1 балл"))
+        rating = 1
+
+    elif data == CALLBACK_BUTTON3_RIGHT:
+        query.edit_message_text(text=(current_text + "\t#оценка: 2 балла"))
+        rating = 2
+
+    write_score(nickname=nickname.strip(), rating=rating)
+
+def start_command(update, context):
+    '''Allows users to participate in quiz. They enter their names when asked'''
+    if current_phase == 0:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Здравствуйте! Как вас зовут?")
+
+        return set_nickname
+    
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Регистрация пока закрыта, пожалуйста, дождитесь окончания текущего раунда")
+
+# chat_id is negative if chat is a group, positive if it's a single person "chat"(and equals to user_id in that case)
+
+def user_nickname(update, context):
+    '''Ask the user to choose a nickname'''
+    user = update.message.from_user
+    nickname = update.message.text
+    user_id = update.message.from_user.id
+    chat_id = update.message.chat.id
+    actual_name = user.first_name
+    logger.info("User_id %s registered: in chat_id %s", user_id, chat_id)
+    logger.info("User %s has chosen nickname: %s", actual_name, nickname)
+
+    if signup(
+        actual_name=actual_name,
+        user_id=user_id,
+        chat_id=chat_id,
+        nickname=nickname,
+        answer=0,
+        rating=0,
+        score=0) == 'OK':
+
+        update.message.reply_text(f"Спасибо, {nickname}, удачи вам!")
+        return ConversationHandler.END
+    
+    else:
+        update.message.reply_text("Пользователь с таким идентификатором или никнеймом уже зарегистрировался")
+        return ConversationHandler.END
+
+def invalid_input(update, context):
+    update.message.reply_text("Неправильный ввод, попробуйте еще раз, пожалуйста")
+
+
+def admquestion_command(update, context):
+    if update.message.from_user.id == admin_id:
+
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Какой зададим вопрос?")
+
+        return question_state
+    
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Эта команда только для администраторов")
+
+
+def admquestion(update,context):
+    global current_phase
+    question = update.message.text
+
+    for i in get_chat_ids():
+        if i != admin_id:
+            context.bot.send_message(chat_id=i, text=question)
+
+            context.bot.pin_chat_message(chat_id=i, message_id=update.message.message_id, disable_notification=None, timeout=None)
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Я разослал вопрос всем участникам, хорошего вам дня")
+    current_phase = 1
+    return ConversationHandler.END
+
+def answers(update, context):
+    if current_phase == 0: pass
+
+    else:
+        msg = str(update.message.text)
+
+        if msg.startswith("#answer"):
+
+            user_id=update.message.from_user.id
+
+            if did_they_answer(user_id=user_id) == 0:
+
+                update.message.reply_text("Ответ принят, спасибо!")
+                get_nick = write_answers(user_id=user_id)
+                context.bot.send_message(chat_id=admin_id, text=get_nick+' '+msg, reply_markup=get_base_inline_keyboard())
+
+            else: update.message.reply_text("Извините, но вы уже дали свой ответ")
+
+        else: pass
+
+def admendround_command(update, context):
+    '''Announce rating of everyone who scored 1 or 2 points this turn'''
+
+    global current_phase
+    results = round_rating()
+    postman = get_chat_ids() # Chat_id's of those who *actually* participated!! And only unique ones.
+    print(results)
+
+    for i in postman:
+        context.bot.send_message(chat_id=i, text=f'Набравшие 2 балла за этот раунд: {results[0]}')
+        context.bot.send_message(chat_id=i, text=f'А вот те, кто набрал 1 балл за этот раунд: {results[1]}')
+
+    current_phase = 0
+
+def score_command(update, context):
+
+    results = total_score()
+
+    flat_list = []
+    for sublist in results:
+        for item in sublist:
+            flat_list.append(item)    
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text=" ".join(str(x) for x in flat_list))
+
+def admreset_command(update, context):
+
+    if update.message.from_user.id == admin_id:
+
+        delete_score()
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Все данные удалены!")
+        init_db() #Запускаем новую пустую таблицу
+    
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Эта команда только для администраторов")
+    
+
+def main():
+    logger.info("Starting the bot")
+
+    req = Request(
+        connect_timeout=1.0, # 0.5
+        read_timeout=2.0,    # 1.0
+    )
+
+    # If a pre-initialized bot is used, it is the user’s responsibility to create it using a Request instance with a large enough connection pool.
+    # Add base_url param if you need to use a proxy
+
+
+    bot = Bot(
+        token=bot_token,
+        request=req,
+    )
+
+    updater = Updater(bot=bot, use_context=True)
+
+    # Проверка, подключились или нет
+    info = bot.get_me()
+    logger.info(f'Bot info: {info}')
+
+    # Подключаем бд
+    init_db()
+
+    dp = updater.dispatcher
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start_command)],
+        states={
+            set_nickname: [MessageHandler(Filters.text & ~Filters.command, user_nickname)]
+        },
+        fallbacks=[MessageHandler(Filters.command, invalid_input)]
+    )
+
+    admquestion_handler = ConversationHandler(
+        entry_points=[CommandHandler('question', admquestion_command)],
+        states={
+            question_state: [MessageHandler(Filters.text & ~Filters.command, admquestion)]
+        },
+        fallbacks=[MessageHandler(Filters.command, invalid_input)]
+    )
+
+    dp.add_handler(conv_handler)
+    dp.add_handler(admquestion_handler)
+    dp.add_handler(CommandHandler('round', admendround_command))
+    dp.add_handler(CommandHandler('score', score_command))
+    dp.add_handler(CommandHandler('reset', admreset_command))
+    dp.add_handler(MessageHandler(Filters.text, answers))
+
+    buttons_handler = CallbackQueryHandler(callback=keyboard_callback_handler, pass_chat_data=True)
+    dp.add_handler(buttons_handler)
+
+    # To start the bot, run:
+    updater.start_polling()
+    updater.idle()
+    logger.info('Bot has been stopped')
+
+if __name__ == "__main__":
+    main()
